97cf6e99af364ea5da6315ae5f695ff816670130
20omkale <omkalepatil20@gmail.com>
2026-05-25 14:49:11 +0530

    feat(api): build json serializers for database models

---
+++ emissions/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Company, DataSource, EmissionRecord
+
+class EmissionRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmissionRecord
+        fields = '__all__'
+
+class DataSourceSerializer(serializers.ModelSerializer):
+    # This allows us to fetch a data source and see all its associated records in one go
+    records = EmissionRecordSerializer(many=True, read_only=True, source='emissionrecord_set')
+    
+    class Meta:
+        model = DataSource
+        fields = '__all__'
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
